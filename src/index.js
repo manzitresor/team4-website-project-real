30342b6bae5b25a260aaf8b6522817eefc757757
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-06-03 17:33:33 +0200

    Fx: get data from javascript

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-function checkme() {
+function service() {
+        const data = ['Brand strategy','Corporate Identity & Website development','SEO & copywriting','Media Relations','Digital marketing & content production','Influencer relations'];
         let service =  document.querySelector("#service>ul");
-        console.log(service);
-
-        
-      
+        const fragment = new DocumentFragment();
+        for( const x of data) {
+                const li = document.createElement('li');
+                li.innerText = x;
+                fragment.append(li);
+        }
+        service.append(fragment);
 }
 
 
