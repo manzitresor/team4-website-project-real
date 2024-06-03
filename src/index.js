844bbbd5a914d456928b8271dd9a42e93ae6829e
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-06-03 18:37:15 +0200

    Fix: Change remove hardcode list of items

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 function service() {
-        const data = ['Brand strategy','Corporate Identity & Website development','SEO & copywriting','Media Relations','Digital marketing & content production','Influencer relations'];
-        let service =  document.querySelector("#service>ul");
-        const fragment = new DocumentFragment();
-        for( const x of data) {
-                const li = document.createElement('li');
-                li.innerText = x;
-                fragment.append(li);
-        }
-        service.append(fragment);
+  const data = [
+    "Brand strategy",
+    "Corporate Identity & Website development",
+    "SEO & copywriting",
+    "Media Relations",
+    "Digital marketing & content production",
+    "Influencer relations",
+  ];
+  let service = document.querySelector("#service>ul");
+  const fragment = new DocumentFragment();
+  for (const x of data) {
+    const li = document.createElement("li");
+    li.classList.add("font-semibold","w-[80%]","md:w-[50%]","lg:w-full");
+    li.innerText = x;
+    fragment.append(li);
+  }
+  service.append(fragment);
 }
-
-
-
