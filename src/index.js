9f9d32590b1c2a51516913af30e7628b057132f9
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-06-04 15:24:27 +0200

    fix: moved contact data to the global scope

--- src/index.js
+++ src/index.js
@@ -1,21 +1,59 @@
+const contactInfo = [
+        {
+                sectionTitle: "Agentur Baumeister",
+                details: [
+                        { type: "p", content: "Katrein Baumeister", class: "py-1" },
+                        { type: "p", content: "Böckhstraße 13", class: "py-1" },
+                        { type: "p", content: "10967 Berlin", class: "py-1" },
+                ],
+        },
+        {
+                sectionTitle: null,
+                details: [
+                        {
+                                type: "a",
+                                content: "+49 (0)30 - 490 827 87",
+                                href: "tel:+493049082787",
+                                class: " py-1",
+                        },
+                        {
+                                type: "br",
+                        },
+                        {
+                                type: "a",
+                                content: "hallo@agentur-baumeister.com",
+                                href: "mailto:hallo@agentur-baumeister.com",
+                                class: "text-wrap  py-1",
+                        },
+                ],
+        },
+        {
+                sectionTitle: "UST-IDNR.",
+                details: [{ type: "p", content: "DE 274 486 923", class: "py-1" }],
+        },
+        {
+                sectionTitle: "REGISTERED OFFICE",
+                details: [{ type: "p", content: "Berlin, Germany", class: "py-1" }],
+        },
+];
 function service() {
-  const data = [
-    "Brand strategy",
-    "Corporate Identity & Website development",
-    "SEO & copywriting",
-    "Media Relations",
-    "Digital marketing & content production",
-    "Influencer relations",
-  ];
-  let service = document.querySelector("#service>ul");
-  const fragment = new DocumentFragment();
-  for (const x of data) {
-    const li = document.createElement("li");
-    li.classList.add("font-semibold");
-    li.innerText = x;
-    fragment.append(li);
-  }
-  service.append(fragment);
+        const data = [
+                "Brand strategy",
+                "Corporate Identity & Website development",
+                "SEO & copywriting",
+                "Media Relations",
+                "Digital marketing & content production",
+                "Influencer relations",
+        ];
+        let service = document.querySelector("#service>ul");
+        const fragment = new DocumentFragment();
+        for (const x of data) {
+                const li = document.createElement("li");
+                li.classList.add("font-semibold");
+                li.innerText = x;
+                fragment.append(li);
+        }
+        service.append(fragment);
 }
 function checkme() {
         let service = document.querySelector("#service>ul");
@@ -26,49 +64,7 @@ function checkme() {
 }
 
 function contactRender() {
-        const contactInfo = [
-                {
-                        sectionTitle: "Agentur Baumeister",
-                        details: [
-                                { type: "p", content: "Katrein Baumeister", class: "py-1" },
-                                { type: "p", content: "Böckhstraße 13", class: "py-1" },
-                                { type: "p", content: "10967 Berlin", class: "py-1" },
-                        ],
-                },
-                {
-                        sectionTitle: null,
-                        details: [
-                                {
-                                        type: "a",
-                                        content: "+49 (0)30 - 490 827 87",
-                                        href: "tel:+493049082787",
-                                        class: " py-1",
-                                },
-                                {
-                                        type: "br",
-                                },
-                                {
-                                        type: "a",
-                                        content: "hallo@agentur-baumeister.com",
-                                        href: "mailto:hallo@agentur-baumeister.com",
-                                        class: "text-wrap  py-1",
-                                },
-                        ],
-                },
-                {
-                        sectionTitle: "UST-IDNR.",
-                        details: [{ type: "p", content: "DE 274 486 923", class: "py-1" }],
-                },
-                {
-                        sectionTitle: "REGISTERED OFFICE",
-                        details: [{ type: "p", content: "Berlin, Germany", class: "py-1" }],
-                },
-        ];
-
-        // document.addEventListener("DOMContentLoaded", () => { });
         const container = document.querySelector(".contacts");
-
-        console.log(container.firstElementChild.previousSibling);
 
         if (!container) {
                 console.error("Container element not found");
@@ -106,8 +102,6 @@ function contactRender() {
 
                 container.append(sectionDiv);
         });
-
-
 }
 
 
